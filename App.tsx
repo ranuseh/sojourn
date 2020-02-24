2429fb43c51e19ce8f37a334cc74c26bb984df3b
ranuseh <rosalyn.anuseh@gmail.com>
2020-02-23 21:54:00 -0800

    Added phone auth

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import PhoneAuth from "./components/phoneAuth";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("./assets/sojournLogo.png")} />
+      <PhoneAuth/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
